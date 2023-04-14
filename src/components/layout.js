7c18d2a0ee1d579d777797ef8a59b57abedeb3f2
laura.vlasma <laura.vlasma@AWI22NB05>
2023-04-14 09:19:09 +0200

    offical-icon-size-fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 import '../styles/index.scss'
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
 
 const Layout = (props) => {
+
     return (
         <>
             <Header />
