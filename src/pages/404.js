1b35c4acdbf37d64d0e3d71d5f2f4023bead90ac
laura.vlasma <laura.vlasma@AWI22NB05>
2023-04-26 13:49:41 +0200

    404-page-update

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from 'gatsby'
+import Aside from '../components/aside'
 import Layout from '../components/layout'
 
 const NotFound = () => {
     return (
         <Layout>
-            <h1>Pagina niet gevonden...</h1>
-            <Link to='/'>Naar startpagina</Link>
+            <main class="post-content">
+                <div>
+                    <h1>🤔 Pagina niet gevonden...</h1>
+                    <p>Niet getreurd! Er zijn genoeg andere verhalen te lezen. Ga terug naar de start pagina om ze allemaal te bekijken.</p>
+                    <Link to='/'>Terug naar startpagina</Link>                  
+                </div>
+                <Aside />
+            </main>
         </Layout>
     )
 }
