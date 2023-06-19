466695b53c1a9c3dd6f5cba8cfbf03be301e8a32
laura.vlasma <laura.vlasma@AWI22NB05>
2023-06-19 10:20:06 +0200

    gitignore-update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,27 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
+
+
+export const onClientEntry = () => {
+    setTimeout(() => {
+      if (document.getElementById("loader-wrapper")) {
+        document.getElementById("loader-wrapper").style.display = "flex";
+      }
+    }, 0);
+  };
+  
+  export const onInitialClientRender = () => {
+    setTimeout(() => {
+      if (document.getElementById("loader-wrapper")) {
+        document.getElementById("loader-wrapper").style.display = "flex";
+      }
+    }, 0);
+  };
+  
+  export const onRouteUpdate = () => {
+    setTimeout(() => {
+      if (document.getElementById("loader-wrapper")) {
+        document.getElementById("loader-wrapper").style.display = "none";
+      }
+    }, 3500);
+  };
+  
