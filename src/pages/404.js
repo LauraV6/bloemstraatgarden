5b40508d50a4a548d944d33a93ed910f3b812635
Laura <lauravlasma1@gmail.com>
2024-01-10 17:44:41 +0100

    404 page edit

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import Sidebar from '../components/sidebar'
 import Layout from '../components/layout'
 import { Helmet } from 'react-helmet'
+import heroImage from "../images/404.png";
 
 const NotFound = () => {
     return (
@@ -10,14 +11,15 @@ const NotFound = () => {
             <Helmet>
                 <title>Bloemstraat pagina niet gevonden</title>
             </Helmet>
-            <main className="post-content">
+            <section className="hero post-hero" style={{ backgroundImage: `url(${heroImage})` }}></section>
+            <section className="post-content">
                 <div>
                     <h1>🤔 Pagina niet gevonden...</h1>
                     <p>Niet getreurd! Er zijn genoeg andere verhalen te lezen. Ga terug naar de start pagina om ze allemaal te bekijken.</p>
                     <Link to='/'>Terug naar startpagina</Link>                  
                 </div>
                 <Sidebar />
-            </main>
+            </section>
         </Layout>
     )
 }
