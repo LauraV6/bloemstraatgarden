cfc0b2c175b58630dc26bc67018f5b140edb7af2
Laura <lauravlasma1@gmail.com>
2024-01-30 15:12:07 +0100

    Update words

--- src/components/header.js
+++ src/components/header.js
@@ -29,12 +29,12 @@ const Header = () => {
         <header>
             <nav>
                 <div className='header-socials'>
-                    <a className='header-link share-icon whapp' href="https://api.whatsapp.com/send?text=https://bloemstraatgarden.netlify.app/" data-action="share/whatsapp/share" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faWhatsapp}/><span>Delen</span></a>
-                    <a className='header-link share-icon linkedin' href="https://www.linkedin.com/in/laura-vlasma-0692b0159/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn}/><span>Delen</span></a>
-                    <a className='header-link share-icon insta' href="https://www.instagram.com/lauravlasma/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram}/><span>Delen</span></a>
+                    <a className='header-link share-icon whapp' href="https://api.whatsapp.com/send?text=https://bloemstraatgarden.netlify.app/" data-action="share/whatsapp/share" target="_blank" rel="referrer noopener"><FontAwesomeIcon icon={faWhatsapp}/><span>Delen</span></a>
+                    <a className='header-link share-icon linkedin' href="https://www.linkedin.com/in/laura-vlasma-0692b0159/" target="_blank" rel="referrer noopener"><FontAwesomeIcon icon={faLinkedinIn}/><span>Delen</span></a>
+                    <a className='header-link share-icon insta' href="https://www.instagram.com/lauravlasma/" target="_blank" rel="referrer noopener"><FontAwesomeIcon icon={faInstagram}/><span>Delen</span></a>
                 </div>
                 <Link to='/' className='logo'><img src={logo} alt="logo"></img></Link>
-                <a className='header-link share-contact' href='https://lauravlasma-portfolio.netlify.app/' target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFolderOpen}/>Toon portfolio</a>
+                <a className='header-link share-contact' href='https://lauravlasma-portfolio.netlify.app/' target="_blank" rel="referrer noopener"><FontAwesomeIcon icon={faFolderOpen}/>Toon portfolio</a>
             </nav>
         </header>
         </>
