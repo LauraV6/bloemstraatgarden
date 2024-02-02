4f23085eef33dbfeb773ab1fe94cfd3da73cef25
Laura <lauravlasma1@gmail.com>
2024-02-02 16:21:31 +0100

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
-        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       }
     },
     `gatsby-plugin-sass`,
