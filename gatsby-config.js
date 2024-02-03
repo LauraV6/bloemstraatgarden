6ea98eb2dbb91c21b82fd0356ba1356d386d9e57
Laura <lauravlasma1@gmail.com>
2024-02-03 22:12:16 +0100

    remove preview

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,7 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       }
     },
     `gatsby-plugin-sass`,
