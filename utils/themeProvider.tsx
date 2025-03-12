7183ec7b9c51fa8c4a24cfcf992229627b2ef28a
Laura V6 <lauravlasma1@gmail.com>
2025-03-12 14:02:35 +0100

    [BUGFIX] ThemeProvider children

--- utils/themeProvider.tsx
+++ utils/themeProvider.tsx
@@ -1,5 +1,11 @@
 "use client";
-import {ThemeProvider as NextThemesProvider} from "next-themes";
-export default function ThemeProvider({children, ...props}) {
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
+
+interface Props {
+  children? : ReactNode
+}
+
+export default function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
