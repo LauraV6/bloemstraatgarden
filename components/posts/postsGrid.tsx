3fa3382c9eff9e0a09c0b8114621cbc2962b9b6c
Laura Vlasma <l.vlasma@inpactplatform.nl>
2025-04-04 12:44:50 +0200

    [REMOVE] api text

--- components/posts/postsGrid.tsx
+++ components/posts/postsGrid.tsx
@@ -1,8 +1,10 @@
 import { getAllArticles } from "@/../lib/api";
 import PostsMap from "./postsMap";
+import { draftMode } from "next/headers";
 
 export default async function PostsGrid() {
 
+    const { isEnabled } = await draftMode();
     const articles = await getAllArticles();
 
     return (
