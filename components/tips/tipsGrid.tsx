0c6c54db7c3dfbed2a4265a1b3012b13bc532173
Laura Vlasma <l.vlasma@inpactplatform.nl>
2025-04-04 09:49:37 +0200

    [REMOVE] Draft

--- components/tips/tipsGrid.tsx
+++ components/tips/tipsGrid.tsx
@@ -1,11 +1,9 @@
 import styles from "./tipsGrid.module.scss";
 import { TipCard } from './tipCard';
 import { getAllTips } from "@/../lib/api";
-import { draftMode } from "next/headers";
 
 export default async function TipsGrid() {
-    const { isEnabled } = await draftMode();
-    const tips = await getAllTips(5, isEnabled);
+    const tips = await getAllTips();
 
     return (
         <div className={styles.tipsGrid}>
