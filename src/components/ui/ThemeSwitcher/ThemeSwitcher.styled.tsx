1126d1b2918b3be866660746ffa153e834cd03d1
Laura Vlasma <l.vlasma@inpactplatform.nl>
2025-09-25 09:43:33 +0200

    Refactor components

--- src/components/ui/ThemeSwitcher/ThemeSwitcher.styled.tsx
+++ src/components/ui/ThemeSwitcher/ThemeSwitcher.styled.tsx
@@ -14,37 +14,12 @@ export const ThemeSwitch = styled.button`
   line-height: 1;
   padding: 0.5rem 1rem;
   margin-left: auto;
-  color: ${props => props.theme.colors.primary};
-  transition: all 0.3s ease;
-  border-radius: 40px;
-  border: 1px solid ${props => props.theme.colors.primary};
-  background-color: transparent;
-  display: flex;
-  align-items: center;
   gap: 0.4rem;
-  font-size: 0.9rem;
 
   @media (max-width: ${props => props.theme.breakpoints.sm}) {
     position: absolute;
     right: 1rem;
     top: 1rem;
-  }
-
-  &:hover {
-    background-color: ${props => props.theme.colors.primary};
-    color: white !important;
-
-    span {
-      color: white !important;
-    }
-
-    svg {
-      color: white !important;
-      
-      path {
-        fill: white !important;
-      }
-    }
   }
 `;
 
