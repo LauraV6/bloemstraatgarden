e180f62d6ad51896b6ca9a1055b822756252d2d6
Laura Vlasma <l.vlasma@inpactplatform.nl>
2025-09-25 09:43:33 +0200

    Refactor components

--- src/components/ui/styled/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './buttons.styled';
-export * from './cards.styled';
-export * from './typography.styled';
-export * from './forms.styled';
-export * from './layout.styled';
-export * from './animations.styled';
-export * from './mixins.styled';
