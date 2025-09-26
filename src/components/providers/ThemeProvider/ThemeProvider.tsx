da63cc91442dba135d4d0a87beeca3490506c84b
Laura Vlasma <l.vlasma@inpactplatform.nl>
2025-09-26 11:36:56 +0200

    Update NextJS version

--- src/components/providers/ThemeProvider/ThemeProvider.tsx
+++ src/components/providers/ThemeProvider/ThemeProvider.tsx
@@ -4,12 +4,13 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider 
+    <NextThemesProvider
       attribute="data-theme"
-      defaultTheme="light"
-      enableSystem={false}
-      disableTransitionOnChange={false}
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
       storageKey="theme"
+      enableColorScheme
     >
       {children}
     </NextThemesProvider>
